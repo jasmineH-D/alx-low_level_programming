84899b8a9fd5bc0dc8fb5227e930eb7f1f1d0dfa
jasmineH-D <mellyasminita2000@gmail.com>
2023-02-16 02:58:09 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <studio.h>
+/**
+*main - Entry point
+*Return: Always 0 (Sucess)
+*/
+
+int main(void)
+	{
+		 puts("\"Programming is like bulding a multilingual puzzle");
+		                  return (0);
