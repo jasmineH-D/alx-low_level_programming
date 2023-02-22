d9943fa4452a4242f04f1e3e4a60af77c0f77ea1
jasmineH-D <mellyasminita2000@gmail.com>
2023-02-21 16:49:11 -0800

    the natural numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints The sum of these multiples
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int a, b = 0;
+
+	while (a < 1024)
+	{
+	if ((i % 3 == 0) || (i % 5 == 0))
+	{
+	b += a;
+	}
+	a++;
+	}
+	printf("%d\n", b);
+	return (0);
+}
