3ab133810102f7c7e9f165d7603c6e26ff2ff45b
jasmineH-D <mellyasminita2000@gmail.com>
2023-02-23 02:51:58 -0800

    Collaboration is multiplication

---
+++ 2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - Multplies two integers
+ * @x: The first number
+ * @y: The second number
+ * Return: The result os multiplication
+ */
+int mul(int a, int b)
+{
+        return (x * y);
+}
