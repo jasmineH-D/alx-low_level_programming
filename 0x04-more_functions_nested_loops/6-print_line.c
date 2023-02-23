982ccb85bb3f812523cbf8779b9fb80c24f374e3
jasmineH-D <mellyasminita2000@gmail.com>
2023-02-23 07:45:07 -0800

    The shortest distance

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * main - check the code
+ * @n: input number
+ * Return: staight line
+ */
+void print_line(int n);
+{
+int y;
+
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (y = 1; y <= n; y++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
+}
