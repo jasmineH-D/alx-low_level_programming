cbb08ceac12c3867b43d77be596449990fed6732
jasmineH-D <mellyasminita2000@gmail.com>
2023-02-23 05:52:58 -0800

    Numbers constitute

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * more_numbers - numbers fonction
+ *
+ * Return: no return
+ */
+void more_numbers(void)
+{
+int i, j;
+for (i = 1; i <= 10; i++)
+{
+for (i = 0; i < 15; j++)
+{
+if (j >= 10)
+_putchar('1');
+_putchar(j % 10 + '0');
+}
+_putchar('\n');
+}
+}
