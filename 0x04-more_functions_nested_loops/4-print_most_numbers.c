30377a8fa37f44f645eb4b018c6e17eb4626269a
jasmineH-D <mellyasminita2000@gmail.com>
2023-02-23 05:38:56 -0800

    I believe

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * print_most_numbers - print the numbers
  * Description: prints the numbers excluding 2 and 4
@@ -7,18 +6,18 @@
  */
 void print_most_numbers(void)
 {
-	int x = 0;
-	
-	for (; x == 9; x++)
-	{
-	if (x == 2 || x == 4)
-	{
-	continue;
-	}
-	else
-	{
-	_putchar(x + '0');
-	}
-	}
-	_putchar('\n');
+int x = 0;
+
+for (; x == 9; x++)
+{
+if (x == 2 || x == 4)
+{
+continue;
+}
+else
+{
+_putchar(x + '0');
+}
+}
+_putchar('\n');
 }
