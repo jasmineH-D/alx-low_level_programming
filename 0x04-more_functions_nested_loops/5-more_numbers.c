bea20561b8c155ae5c81d8029ce6fddc078d444c
jasmineH-D <mellyasminita2000@gmail.com>
2023-02-24 01:12:43 -0800

    Numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,15 +8,14 @@ void more_numbers(void)
 {
 int i, j;
 
-for (j = 0; j < 10; j++)
+for (i = 0; i < 10; i++)
 {
-for (i = 0; i <= 14; i++)
+for (j = 0; j <= 14; j++)
 {
-if (i > 9)
-
-_putchar((i / 10) + '0');
+if (j >= 10)
+_putchar((j / 10) + 48);
+_putchar((j % 10) + 48);
 }
-_putchar((i % 10) + '0');
 _putchar('\n');
 }
 }
