0f834a16d0382d51434486c5a6a6c71ac5b14a90
jasmineH-D <mellyasminita2000@gmail.com>
2023-02-27 05:24:30 -0800

    I can only go one way.

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <unistd.h>
 
+void _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -15,6 +16,5 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
-int _putchar(char c);
 
 #endif
