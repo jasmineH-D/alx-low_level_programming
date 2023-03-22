ce93119e9f787d5fb32baf60971561925e6824a8
jasmineH-D <mellyasminita2000@gmail.com>
2023-03-22 07:35:01 -0700

    task4

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,37 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
-* main - prints its own opcodes
-* @argc: number of arguments
-* @argv: array of arguments
-*
-* Return: Always 0 (Success)
-*/
+ * main - check the code for ALX students.
+ * @argc: argument count.
+ * @argv: argument vector.
+ *
+ * Return: Always 0.
+ */
 int main(int argc, char *argv[])
 {
-                int bytes, i;
-                char *arr;
+	char *opc = (char *) main;
+	int i, nbytes;
 
-                if (argc != 2)
-                {
-                        printf("Error\n");
-                        exit(1);
-                }
-                bytes = atoi(argv[1]);
-                if (bytes < 0)
-                {
-                        printf("Error\n");
-                        exit(2);
-                }
-                arr = (char *)main;
-                for (i = 0; i < bytes; i++)
-                {
-                        if (i == bytes - 1)
-                        {
-                                printf("%02hhx\n", arr[i]);
-                                break;
-                        }
-                        printf("%02hhx ", arr[i]);
-                }
-                return (0);
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	nbytes = atoi(argv[1]);
+
+	if (nbytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (i = 0; i < nbytes; i++)
+	{
+		printf("%02x", opc[i] & 0xFF);
+		if (i != nbytes - 1)
+			printf(" ");
+	}
+
+	printf("\n");
+	return (0);
 }
