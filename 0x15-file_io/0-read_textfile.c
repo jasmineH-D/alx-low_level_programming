c23269d19d157eb8348cd557abc49a44c38f248f
jasmineH-D <mellyasminita2000@gmail.com>
2023-04-14 06:00:06 -0700

    task0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,7 +16,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (!filename)
 		return (0);
 
-	buf = malloc(sizeof(char) * latters);
+	buf = malloc(sizeof(char) * letters);
 	if (!buf)
 		return (0);
 
