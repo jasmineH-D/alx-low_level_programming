59a8e28a5b283b0e8e90f9533f5fe64630033f16
jasmineH-D <mellyasminita2000@gmail.com>
2023-04-14 08:42:45 -0700

    task2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -14,9 +14,12 @@ int append_text_to_file(const char *filename, char *text_content)
 	int fd, by, len = 0;
 
 	if (!filename)
+		return (-1);
+	if (text_content != NULL)
+	{
 	while (text_content[len])
 		len++;
-
+	}
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd < 0)
 		return (-1);
