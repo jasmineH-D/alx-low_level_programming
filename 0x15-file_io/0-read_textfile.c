9394100a111e6af2b9802a5eec508a5a68c87d5c
jasmineH-D <mellyasminita2000@gmail.com>
2023-05-09 04:45:26 -0700

    task0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,37 +1,36 @@
-#include "shell.h"
+#include "main.h"
 
 /**
- * read_textfile - read text and print it to POSIX
- * @filename: name of file char
- * @letters:number of letters to read size_t
- * Return: read the file display it or error 0
- */
+ * read_textfile - reads a txt prints it to the POSIX standard output
+ * @filename: filename
+ * @letters: letters
+ * Return: num of letters or 0
+*/
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, rd, wd;
-	char *buff = malloc(sizeof(char) * letters);
+	int the_F, the_R, the_W;
+	char *map = malloc(letters * sizeof(char));
 
-	if (filename == NULL)
-	{
+	if (filename == NULL || letters == 0 || map == NULL)
 		return (0);
-	}
 
-	fd = open(filename, O_RDWR);
-	if (fd == -1)
+	the_F = open(filename, O_RDONLY);
+	if (the_F == -1)
+		return (0);
+	the_R =  read(the_F, map, letters);
+	if (the_R == -1)
 	{
+		free(map);
 		return (0);
 	}
-	rd = read(fd, buff, letters);
-	if (rd == -1)
+	the_W = write(STDOUT_FILENO, map, the_R);
+
+	if (the_W < 0)
 	{
+		free(map);
 		return (0);
 	}
-	wd = write(STDOUT_FILENO, buff, rd);
-	if (wd == -1)
-	{
-		return (0);
-	}
-	close(fd);
-	free(buff);
-	return (wd);
+	close(the_F);
+	return (the_R);
 }
