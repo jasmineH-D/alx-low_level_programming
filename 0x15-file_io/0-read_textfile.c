825f1d397a504fe7c3d7724b154f0b9a1911f9cf
jasmineH-D <mellyasminita2000@gmail.com>
2023-05-09 05:13:44 -0700

    task0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,28 +9,18 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int the_F, the_R, the_W;
-	char *map = malloc(letters * sizeof(char));
+	int fd;
+	char buf[READ_BUF_SIZE * 8];
+	ssize_t bytes;
 
-	if (filename == NULL || letters == 0 || map == NULL)
+	if (!filename || !letters)
 		return (0);
 
-	the_F = open(filename, O_RDONLY);
-	if (the_F == -1)
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 		return (0);
-	the_R =  read(the_F, map, letters);
-	if (the_R == -1)
-	{
-		free(map);
-		return (0);
-	}
-	the_W = write(STDOUT_FILENO, map, the_R);
-
-	if (the_W < 0)
-	{
-		free(map);
-		return (0);
-	}
-	close(the_F);
-	return (the_R);
+	bytes =  read(fd, &buf[0], letters);
+	bytes =  write(STDOUT_FILENO, &buf[0], bytes);
+	close(fd);
+	return (bytes);
 }
